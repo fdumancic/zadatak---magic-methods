3dfccfa23d479a6788ee3616b24e70e4cfe14464
fdumancic <dumancic.filip@gmail.com>
2018-02-26 14:00:27 +0100

    17-18

--- src/Contracts/Base.php
+++ src/Contracts/Base.php
@@ -2,21 +2,30 @@
 
 namespace App\Contracts;
 
-
 class Base
 {
 	protected $data = [];
 
-	protected $hidden = ['slug', 'id'];
+	protected $hidden = ['slug'];
 
 	protected $appends = ['full_name'];
+
+    protected $fillable = ['first_name', 'last_name'];
+
+    protected $guarded = ['id'];
 
 	public function __construct(array $arguments = [])
 	{
 		$this->data = $arguments;
 	}
 
-
+    /**
+     * Magic method __callStatic()
+     *
+     * @param string $name
+     * @param array $arguments
+     * @return object
+     */
 	public static function __callStatic($name, $arguments)
     {
     	$class_name = get_called_class();
@@ -24,6 +33,13 @@ class Base
 	    return call_user_func_array(array($class, $name), $arguments);
     }
 
+    /**
+     * Magic method __call()
+     *
+     * @param string $name
+     * @param array $arguments
+     * @return object
+     */
     public function __call($name, $arguments)
     {
     	if($name == 'create'){
@@ -31,6 +47,12 @@ class Base
     	}
     }
 
+    /**
+     * Creates instance of current class
+     *
+     * @param array $arguments
+     * @return object
+     */
     protected function create(array $arguments)
     {
     	$new_class = get_class($this);
@@ -38,64 +60,83 @@ class Base
     	return $model;
     }
 
+    /**
+     * Returns array with appended and without hidden elements
+     *
+     * @return array
+     */
     public function toArray()
     {
-    	/*$new_array = (array)$this->data;
-    	$hidden_array = (array)$this->hidden;
-
-    	foreach ($hidden_array as $key => $value) {
-    		if(array_key_exists($value, $new_array)) {
-    			unset($new_array[$value]);
-    		}
-    	}
-    	return $new_array;*/
-//-------------------
-    	/*$new_array = (array)$this->data;
-    	$hidden_array = (array)$this->hidden;
-
-    	$filltered = array_diff_key($new_array, array_flip($hidden_array));
-
-    	return $filltered;*/
-//-------------------
     	$new_array = (array)$this->data;
     	$hidden_array = $this->hidden;
-    	$appends_array = $this->appends;
+        $to_merge = [];
 
-    	$filtered_array = array_filter($new_array, function ($key) use ($hidden_array) {
-    		return !in_array($key, $hidden_array);
-    		}, ARRAY_FILTER_USE_KEY);
-    	return $filtered_array;
+        foreach ($this->appends as $key => $value) {
+            $function_name = 'get'.ucfirst($value).'Attribute';
+            if(method_exists($this, $function_name)) {
+                $to_merge[$value] = call_user_func(array($this, $function_name));
+            }
+        }
 
-    	$appended_array = $filtered_array;
+    	$appended_array = array_merge($new_array, $to_merge);
+
+        $filtered_array = array_filter($appended_array, function ($key) use ($hidden_array) {
+            return !in_array($key, $hidden_array);
+            }, ARRAY_FILTER_USE_KEY);
+        return $filtered_array;
 
     }
 
+    /**
+     * Magic method __set()
+     *
+     * @param string $name
+     * @param string $value
+     * @return object
+     */
     public function __set($name, $value)
     {
-    	$f = 'set'.ucfirst($name).'Attribute';
-    	if(method_exists($this, $f)) {
-	    	return call_user_func(array($this, $f), $value);
+    	$function_name = 'set'.ucfirst($name).'Attribute';
+    	if(method_exists($this, $function_name)) {
+	    	return call_user_func(array($this, $function_name), $value);
     	}
-        $this->data[$name] = $value;
+
+        if(in_array($name, $this->fillable) && (!in_array($name, $this->guarded))){
+            $this->data[$name] = $value;
+        }
     }
 
+    /**
+     * Magic method __get()
+     *
+     * @param string $name
+     * @return object
+     */
     public function __get($name)
     {
-    	$f = 'get'.ucfirst($name).'Attribute';
-    	if(method_exists($this, $f)) {
-	    	return call_user_func(array($this, $f), $this->data[$name]);
+    	$function_name = 'get'.ucfirst($name).'Attribute';
+    	if(method_exists($this, $function_name)) {
+	    	return call_user_func(array($this, $function_name), $this->data[$name]);
     	}
-        if (array_key_exists($name, $this->data)) {
+        if(array_key_exists($name, $this->data)) {
             return $this->data[$name];
         }
     }
-    public function getFullNameAttribute()
+
+    /**
+     * Returns string with full name
+     *
+     * @return string
+     */
+    public function getFull_nameAttribute()
     {
   		return $this->first_name . ' ' . $this->last_name;
 	}
 }
 
-$a = new Base(["id" => "1", "slug" => "test", "first_name" => "John", "last_name" => "Doe"]);
+$a = new Base(["id" => "1", "slug" => "aaabbbccc", "first_name" => "John", "last_name" => "Doe"]);
 $b = $a->toArray();
+$a->test = 'aaaaa';
+$c = $a->test;
 
-print_r($b);
+print_r($c);
